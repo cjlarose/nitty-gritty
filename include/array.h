798c4e567d3bc04a4bb4513deab1e19f46b9cf60
Chris LaRose <cjlarose@iplantcollaborative.org>
2013-10-20 16:47:12 -0700

    Initial commit

---
+++ include/array.h
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+struct {
+    size_t data_length; // size in bytes of single element
+    int length;         // current length of the virtual array
+    int capacity;       // current length of real array
+    void * start;       // ptr to start of real array
+} array;
+
+typedef struct array * Array;
+
+/* Initialize a dynamic array that stores elements of size data_length */
+Array Array_init(size_t data_length);
+
+/* Return the contents at index i */
+void * Array_get(Array arr, int i);
+
+/* Set the contents at index i to element */
+void Array_set(Array arr, int i, void * element);
+
+/* Add an element to the end of the array */
+void Array_append(Array arr, void * element);
