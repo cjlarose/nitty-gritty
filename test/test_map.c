864a10c66fb08ea440e5993e27be4e99a2291663
Chris LaRose <cjlarose@iplantcollaborative.org>
2013-10-30 23:47:54 -0700

    Reformatted map test with minunit

--- test/test_map.c
+++ test/test_map.c
@@ -3,8 +3,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include "map.h"
+#include "minunit.h"
 
-long int str_hash(void *ptr) {
+unsigned long long int str_hash(void *ptr) {
     char *str = (char *) ptr;
     long int h = 0;  
     for (; *str != '\0'; str++) {
@@ -25,16 +26,26 @@ void print_kv(void *ptr1, void **ptr2) {
     printf("%s => %s\n", (char *) ptr1, (char *) *ptr2);
 }
 
-int main() {
-    struct map *my_map = map_new(&str_hash, &str_eq, 4);
+struct map *my_map = NULL;
+char **args = NULL;
+
+char * test_init() {
+    my_map = map_new(&str_hash, &str_eq, 4);
+    return NULL;
+}
+
+char * test_insert() {
     map_insert(my_map, "one", "two");
     map_insert(my_map, "three", "four");
     map_insert(my_map, "five", "six");
     map_insert(my_map, "seven", "eight");
     map_insert(my_map, "nine", "ten");
     map_insert(my_map, "nine", "potato");
+    return NULL;
+}
 
-    char **args = malloc(26 * sizeof(char *));
+char * test_insert_alpha() {
+    args = malloc(26 * sizeof(char *));
     int j;
     for (j = 0; j < 26; j++) {
         char *str = calloc(2, sizeof(char));
@@ -42,7 +53,10 @@ int main() {
         args[j] = str;
         map_insert(my_map, str, str);
     }
+    return NULL;
+}
 
+char * test_find() {
     char *keys[] = {"one", "three", "five", "seven", "nine"};
 
     int i;
@@ -50,13 +64,29 @@ int main() {
         char *value = (char *) *(map_find(my_map, keys[i]));
         printf("%s\n", value);
     }
+    return NULL;
+}
 
+char * test_apply() {
     map_apply(my_map, &print_kv);
+    return NULL;
+}
 
-    map_free(my_map, NULL, NULL);
+char *all_tests() {
+    mu_suite_start();
+    mu_run_test(test_init);
+    mu_run_test(test_insert);
+    mu_run_test(test_insert_alpha);
+    mu_run_test(test_find);
+    mu_run_test(test_apply);
 
+    map_free(my_map, NULL);
+
+    int j;
     for (j = 0; j < 26; j++)
         free(args[j]);
     free(args);
-    return 0;
+    return NULL;
 }
+
+RUN_TESTS(all_tests);
