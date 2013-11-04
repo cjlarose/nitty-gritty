58656df2bba0a18b30ad271685cfb5fced3d8e10
Chris La Rose <cjlarose@email.arizona.edu>
2013-11-03 18:09:16 -0700

    Added tests to for array

--- test/test_array.c
+++ test/test_array.c
@@ -22,9 +22,14 @@ char * test_set_get() {
 
 char * test_append() {
     Array_append(arr1, "five");
+    mu_assert(ARRAY_ELEMENT_EQ(arr1, arr1->length - 1, "five"), "Wrong value");
     Array_append(arr1, "sixx");
+    mu_assert(ARRAY_ELEMENT_EQ(arr1, arr1->length - 1, "sixx"), "Wrong value");
     Array_append(arr1, "sevn");
+    mu_assert(ARRAY_ELEMENT_EQ(arr1, arr1->length - 1, "sevn"), "Wrong value");
     Array_append(arr1, "eiht");
+    mu_assert(ARRAY_ELEMENT_EQ(arr1, arr1->length - 1, "eiht"), "Wrong value");
+
     mu_assert(ARRAY_ELEMENT_EQ(arr1, 4, "five"), "Wrong value");
     mu_assert(ARRAY_ELEMENT_EQ(arr1, 5, "sixx"), "Wrong value");
     mu_assert(ARRAY_ELEMENT_EQ(arr1, 6, "sevn"), "Wrong value");
@@ -37,7 +42,10 @@ char * test_append() {
 char * test_init_with_0() {
     arr2 = Array_init(0, 5);
     Array_append(arr2, "what");
+    mu_assert(ARRAY_ELEMENT_EQ(arr2, arr2->length - 1, "what"), "Wrong value");
     Array_append(arr2, "wutt");
+    mu_assert(ARRAY_ELEMENT_EQ(arr2, arr2->length - 1, "wutt"), "Wrong value");
+
     mu_assert(ARRAY_ELEMENT_EQ(arr2, 0, "what"), "Wrong value");
     mu_assert(ARRAY_ELEMENT_EQ(arr2, 1, "wutt"), "Wrong value");
 
